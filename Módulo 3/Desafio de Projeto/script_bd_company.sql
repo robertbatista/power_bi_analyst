6f4c1b2c59a98c7e9ccc360f8ae3de90a25f9c99
Robert Batista <71283492+robertbatista@users.noreply.github.com>
2024-04-15 19:55:08 -0300

    Update script_bd_company.sql
    modificaçao nos dados

--- Módulo 3/Desafio de Projeto/script_bd_company.sql
+++ Módulo 3/Desafio de Projeto/script_bd_company.sql
@@ -17,10 +17,10 @@ CREATE TABLE employee(
     Sex char,
     Salary decimal(10,2),
     Super_ssn char(9),
-    Dno int not null,
-    constraint chk_salary_employee check (Salary> 2000.0),
-    constraint pk_employee primary key (Ssn)
-);
+    Dno int not null);
+    --constraint chk_salary_employee check (Salary> 2000.0),
+   -- constraint pk_employee primary key (Ssn)
+--);
 
 alter table employee 
 	add constraint fk_employee 
@@ -37,11 +37,11 @@ create table departament(
     Dnumber int not null,
     Mgr_ssn char(9) not null,
     Mgr_start_date date, 
-    Dept_create_date date,
-    constraint chk_date_dept check (Dept_create_date < Mgr_start_date),
-    constraint pk_dept primary key (Dnumber),
-    constraint unique_name_dept unique(Dname),
-    foreign key (Mgr_ssn) references employee(Ssn)
+    Dept_create_date date);
+    --constraint chk_date_dept check (Dept_create_date < Mgr_start_date),
+    --constraint pk_dept primary key (Dnumber),
+    --constraint unique_name_dept unique(Dname),
+    --foreign key (Mgr_ssn) references employee(Ssn)
 );
 
 -- 'def', 'company_constraints', 'departament_ibfk_1', 'company_constraints', 'departament', 'FOREIGN KEY', 'YES'
@@ -55,10 +55,10 @@ desc departament;
 
 create table dept_locations(
 	Dnumber int not null,
-	Dlocation varchar(15) not null,
-    constraint pk_dept_locations primary key (Dnumber, Dlocation),
-    constraint fk_dept_locations foreign key (Dnumber) references departament (Dnumber)
-);
+	Dlocation varchar(15) not null);
+    --constraint pk_dept_locations primary key (Dnumber, Dlocation),
+    --constraint fk_dept_locations foreign key (Dnumber) references departament (Dnumber)
+--);
 
 alter table dept_locations drop fk_dept_locations;
 
@@ -72,20 +72,20 @@ create table project(
 	Pnumber int not null,
     Plocation varchar(15),
     Dnum int not null,
-    primary key (Pnumber),
-    constraint unique_project unique (Pname),
-    constraint fk_project foreign key (Dnum) references departament(Dnumber)
-);
+    primary key (Pnumber));
+    --constraint unique_project unique (Pname),
+    --constraint fk_project foreign key (Dnum) references departament(Dnumber)
+--);
 
 
 create table works_on(
 	Essn char(9) not null,
     Pno int not null,
     Hours decimal(3,1) not null,
-    primary key (Essn, Pno),
-    constraint fk_employee_works_on foreign key (Essn) references employee(Ssn),
-    constraint fk_project_works_on foreign key (Pno) references project(Pnumber)
-);
+    primary key (Essn, Pno));
+    --constraint fk_employee_works_on foreign key (Essn) references employee(Ssn),
+    --constraint fk_project_works_on foreign key (Pno) references project(Pnumber)
+--);
 
 drop table dependent;
 create table dependent(
@@ -94,9 +94,9 @@ create table dependent(
     Sex char,
     Bdate date,
     Relationship varchar(8),
-    primary key (Essn, Dependent_name),
-    constraint fk_dependent foreign key (Essn) references employee(Ssn)
-);
+    primary key (Essn, Dependent_name));
+    --constraint fk_dependent foreign key (Essn) references employee(Ssn)
+--);
 
 show tables;
 desc dependent;
